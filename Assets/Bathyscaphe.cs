c52012e7ffa83e696b9dfcd7e3258331faf0fda6
AndreaCampagner <onyris93@gmail.com>
2017-10-28 11:34:46 +0200

    Fixed and Refactored Movement

--- Assets/Bathyscaphe.cs
+++ Assets/Bathyscaphe.cs
@@ -7,32 +7,43 @@ public class Bathyscaphe : MonoBehaviour {
 
     Rigidbody rigidBody;
     AudioSource audioSource;
+    [SerializeField] float rotationFactor = 100f;
+    [SerializeField] float thrustFactor = 10f;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         rigidBody = gameObject.GetComponent<Rigidbody>();
         audioSource = gameObject.GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        ProcessInput();
-	}
+        Thrust();
+        Rotate();
+    }
 
-    private void ProcessInput()
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; //Manual control taken
+        float rotationThisFrame = rotationFactor * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        else if (Input.GetKey(KeyCode.D))
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+
+        rigidBody.freezeRotation = false; //Manual control released
+    }
+
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up * 10.0f);
+            rigidBody.AddRelativeForce(Vector3.up * thrustFactor);
             if (!audioSource.isPlaying)
                 audioSource.Play();
         }
         else
             audioSource.Stop();
-
-        if (Input.GetKey(KeyCode.A))
-            rigidBody.AddRelativeTorque(Vector3.forward * 0.5f);
-        else if (Input.GetKey(KeyCode.D))
-            rigidBody.AddRelativeTorque(-Vector3.forward * 0.5f);
     }
 }
